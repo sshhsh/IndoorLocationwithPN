8f4a7b9f434971d44adaef65a0b42c5bf17ab4f4
zhucebiao <sshhsh@foxmail.com>
2017-11-22 13:32:32 +0800

    Create CrossCorrelation.java

---
+++ app/src/main/java/com/example/zhucebiao/indoorlocationwithpn/CrossCorrelation.java
@@ -0,0 +1,71 @@
+package com.example.zhucebiao.indoorlocationwithpn;
+
+/**
+ * Created by zhucebiao on 17-11-22.
+ */
+
+import org.jtransforms.fft.DoubleFFT_1D;
+
+public class CrossCorrelation {
+    private static final String TAG = "CrossCorrelation";
+    private double a[];
+    private double b[];
+    private double c[];
+    private double r[];
+    private int len;
+    private DoubleFFT_1D fft;
+
+    public CrossCorrelation(int length) {
+        len = length;
+        fft = new DoubleFFT_1D(length);
+
+        a = new double[len * 2];
+        b = new double[len * 2];
+        c = new double[len * 2];
+        r = new double[len];
+    }
+
+    public double[] getResult(short[] longData, short[] sequence) {
+        if (len != longData.length)
+            return null;
+        for (int i = 0; i < len; ++i) {
+            a[2 * i] = longData[i];
+            a[2 * i + 1] = 0;
+            if (i < sequence.length) b[2 * i] = sequence[i];
+            else b[2 * i] = 0;
+            b[2 * i + 1] = 0;
+        }
+        fft.complexForward(a);
+        //filt(a);
+        fft.complexForward(b);
+        for (int i = 0; i < len; ++i) {
+            b[2 * i + 1] = -b[2 * i + 1];
+        }
+        for (int i = 0; i < len; ++i) {
+            c[2 * i] = a[2 * i] * b[2 * i] - a[2 * i + 1] * b[2 * i + 1];
+            c[2 * i + 1] = a[2 * i] * b[2 * i + 1] + a[2 * i + 1] * b[2 * i];
+        }
+        fft.complexInverse(c, false);
+        for (int i = 0; i < len; ++i) {
+            r[i] = c[2 * i] * c[2 * i] + c[2 * i + 1] * c[2 * i + 1];
+        }
+        return r;
+    }
+
+    private void filt(double[] a) {
+        int t1 = 17000 * len / 44100;
+        int t2 = 22000 * len / 44100;
+        for (int i = 0; i < 2 * t1; ++i) {
+            a[2 * t1] = 0;
+            a[2 * t1 + 1] = 0;
+        }
+        for (int i = 2 * t2; i < 2 * (len - t2); ++i) {
+            a[2 * t1] = 0;
+            a[2 * t1 + 1] = 0;
+        }
+        for (int i = 2 * (len - t1); i < 2 * len; ++i) {
+            a[2 * t1] = 0;
+            a[2 * t1 + 1] = 0;
+        }
+    }
+}
